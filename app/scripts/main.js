78b3643d2ee69ecf56b60f92b6b00876a6366222
shin <shin@nawagate.com>
2016-06-28 19:03:46 +0900

    no message

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -26,51 +26,51 @@
 // common bottom
 (function() {
   'use strict';
-  var $body = $('body'),
-    id = location.hash.replace('#', ''),
-    $target = id
-      ? $($.find('#' + id))
-      : undefined,
-    stopReplacingState = $target && $target.length,
-    $navbarTarget = id
-      ? $($.find('.navbar-right a[href="#' + id + '"]')).parent()
-      : undefined;
-  if (id) {
-    $($.find('body > a[name="' + id + '"]')).remove();
-  }
+  var $body = $('body');
+//     id = location.hash.replace('#', ''),
+//     $target = id
+//       ? $($.find('#' + id))
+//       : undefined,
+//     stopReplacingState = $target && $target.length,
+//     $navbarTarget = id
+//       ? $($.find('.navbar-right a[href="#' + id + '"]')).parent()
+//       : undefined;
+//   if (id) {
+//     $($.find('body > a[name="' + id + '"]')).remove();
+//   }
   $body.scrollspy({target: '.navbar-right', offset: 100});
-  if (history.replaceState) {
-    $('.navbar-right').on('activate.bs.scrollspy', function() {
-      if (stopReplacingState) {
-        return;
-      }
-      var $active = $('.navbar-right .active a');
-      if ($active.is(':visible')) {
-        history.replaceState('', '', $active.prop('href').replace(/.*#/, '#'));
-      } else {
-        history.replaceState('', '', location.href.replace(/#.*/, ''));
-      }
-    });
-  }
-  if (!$target || !$target.length) {
-    return;
-  }
-  $navbarTarget.addClass('force-active');
-  $target.prop('id', '');
-  $(window).load(function() {
-    setTimeout(function() {
-      $target.prop('id', id);
-      var pos = $target.offset().top - 50;
-      $body.addClass('smooth-scroll-scrolling');
-      $('html, body').animate({
-        scrollTop: pos
-      }, 750, 'swing', function() {
-        stopReplacingState = false;
-        $body.removeClass('smooth-scroll-scrolling');
-        $navbarTarget.removeClass('force-active');
-      });
-    }, 300);
-  });
+//   if (history.replaceState) {
+//     $('.navbar-right').on('activate.bs.scrollspy', function() {
+//       if (stopReplacingState) {
+//         return;
+//       }
+//       var $active = $('.navbar-right .active a');
+//       if ($active.is(':visible')) {
+//         history.replaceState('', '', $active.prop('href').replace(/.*#/, '#'));
+//       } else {
+//         history.replaceState('', '', location.href.replace(/#.*/, ''));
+//       }
+//     });
+//   }
+//   if (!$target || !$target.length) {
+//     return;
+//   }
+//   $navbarTarget.addClass('force-active');
+//   $target.prop('id', '');
+//   $(window).load(function() {
+//     setTimeout(function() {
+//       $target.prop('id', id);
+//       var pos = $target.offset().top - 50;
+//       $body.addClass('smooth-scroll-scrolling');
+//       $('html, body').animate({
+//         scrollTop: pos
+//       }, 750, 'swing', function() {
+//         stopReplacingState = false;
+//         $body.removeClass('smooth-scroll-scrolling');
+//         $navbarTarget.removeClass('force-active');
+//       });
+//     }, 300);
+//   });
 })();
 // 共通
 (function() {
